f3cc5a6a3222e63ba2066b57dd79f4c0798a598e
mmattbtw <matt@mmatt.net>
2025-04-03 21:47:59 -0500

    oops

---
+++ src/app/.well-known/webfinger/route.ts
@@ -0,0 +1,22 @@
+export function GET() {
+  return Response.json({
+    subject: "acct:matt@social.lol",
+    aliases: ["https://social.lol/@matt", "https://social.lol/users/matt"],
+    links: [
+      {
+        rel: "http://webfinger.net/rel/profile-page",
+        type: "text/html",
+        href: "https://social.lol/@matt",
+      },
+      {
+        rel: "self",
+        type: "application/activity+json",
+        href: "https://social.lol/users/matt",
+      },
+      {
+        rel: "http://ostatus.org/schema/1.0/subscribe",
+        template: "https://social.lol/authorize_interaction?uri={uri}",
+      },
+    ],
+  });
+}
