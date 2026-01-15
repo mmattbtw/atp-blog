8063042a024f93e6bd15616291d8f84217cedf97
mmattbtw <matt@mmatt.net>
2026-01-14 21:22:08 -0600

    yup

--- src/lib/bsky.ts
+++ src/lib/bsky.ts
@@ -1,9 +1,11 @@
-import { Client, CredentialManager } from "@atcute/client";
+import type {} from "@atcute/bluesky";
+
+import { Client, simpleFetchHandler } from "@atcute/client";
 
 import { env } from "./env";
 
-const handler = new CredentialManager({
-  service: env.NEXT_PUBLIC_BSKY_PDS,
-  fetch,
+const client = new Client({
+  handler: simpleFetchHandler({ service: env.NEXT_PUBLIC_BSKY_PDS }),
 });
-export const bsky = new Client({ handler });
+
+export const bsky = client;
