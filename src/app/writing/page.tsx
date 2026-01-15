1503607a69d45a2842abe2d98269b694f6562eb5
mmattbtw <matt@mmatt.net>
2026-01-14 23:17:44 -0600

    oh you know

--- src/app/writing/page.tsx
+++ src/app/writing/page.tsx
@@ -1,6 +1,5 @@
+import { BlogVideoBackground } from "#/components/home-video-background";
 import { PostList } from "#/components/post-list";
-
-import HomeVideoBackground from "../../components/home-video-background";
 
 // Revalidate every hour (3600 seconds)
 export const revalidate = 3600;
@@ -8,7 +7,7 @@ export const revalidate = 3600;
 export default function WritingPage() {
   return (
     <main className="relative min-h-screen overflow-hidden">
-      <HomeVideoBackground />
+      <BlogVideoBackground />
       <div className="absolute inset-0 z-10 min-h-screen">
         <div className="max-w-148 pt-1.5 pl-1.5 space-y-4">
           <h1 className="font-bold text-black dark:text-white  bg-white dark:bg-black inline">
--- src/components/home-video-background.tsx
+++ src/components/home-video-background.tsx
@@ -13,3 +13,15 @@ export default function HomeVideoBackground() {
     />
   );
 }
+
+export function BlogVideoBackground() {
+  return (
+    <DitheredVideoFilter
+      videoSrc="https://wh9xsi1isi.ufs.sh/f/3pCuYtORE70iFh1vC2kQTmgCPNMpYynkzK4WXJelbq527Zjo"
+      pixelSize={4}
+      maxFPS={12}
+      maxWidth={1000}
+      className="fixed inset-0 w-screen h-screen object-cover -z-10 invert dark:invert-0"
+    />
+  );
+}
