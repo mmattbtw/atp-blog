e1d9eceda3c60c759c39f73ad591e831ed324548
mmattbtw <matt@mmatt.net>
2026-02-04 20:01:03 -0600

    twoob

--- src/app/twoobprivacypolicy/page.tsx
+++ src/app/twoobprivacypolicy/page.tsx
@@ -51,13 +51,13 @@ If this policy changes, the updated version will be posted in this file.
 If you have questions about this policy, contact matt (@) mmatt.net 
 `;
 
-export default function DeerDirectPrivacyPolicyPage() {
+export default function TwoobPrivacyPolicyPage() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] justify-items-center min-h-dvh py-8 px-4 xs:px-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start w-full max-w-3xl overflow-hidden">
         <article className="w-full space-y-8">
           <div className="space-y-4 w-full">
-            <Title className="text-center">DeerDirect Privacy Policy</Title>
+            <Title className="text-center">Twoob Privacy Policy</Title>
             <hr />
           </div>
           <div>
